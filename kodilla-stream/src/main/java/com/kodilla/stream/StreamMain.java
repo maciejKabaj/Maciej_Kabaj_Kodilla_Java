a409d4c968015f324c644f7062c042c74027420b
Maciej Kabaj <maciejkabaj93@gmail.com>
2025-05-15 17:47:48 +0200

    7.2 Added immutable ForumUser class and NumbersGenerator class with Stream basic instructions

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,16 +1,13 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 
 public class StreamMain {
 
     public static void main(String[] args) {
-        PoemBeautifier poemBeautifier = new PoemBeautifier();
-        String myPoem = "WsAd";
-        poemBeautifier.beautify(myPoem, text -> "ABC" + text + "ABC");
-        poemBeautifier.beautify(myPoem, String::toUpperCase);
-        poemBeautifier.beautify(myPoem, String::toLowerCase);
-        poemBeautifier.beautify(myPoem, text -> text.repeat(3));
-        poemBeautifier.beautify(myPoem, text -> new StringBuilder(text).reverse().toString());
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
