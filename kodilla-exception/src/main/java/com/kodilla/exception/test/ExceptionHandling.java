acfb70e1cb1ecf3d301db11e072b74bdb24f37c9
Maciej Kabaj <maciejkabaj93@gmail.com>
2025-05-24 14:40:11 +0200

    8.3 Added SecondChallenge and ExceptionHandling classes with custom exception logic and manual edge case tests

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
@@ -0,0 +1,41 @@
+package com.kodilla.exception.test;
+
+public class ExceptionHandling {
+    public static void main(String[] args) {
+        SecondChallenge secondChallenge = new SecondChallenge();
+
+        //Should throw an exception
+        try {
+            System.out.println(secondChallenge.probablyIWillThrowException(1.5, 1.5));
+        }
+        catch (Exception e) {
+            System.out.println("Program has an issue: " + e);
+        }
+        finally {
+            System.out.println("End of program");
+        }
+
+        //Should return "Done!"
+        try {
+            System.out.println(secondChallenge.probablyIWillThrowException(1.5, 100));
+        }
+        catch (Exception e) {
+            System.out.println("Program has an issue: " + e);
+        }
+        finally {
+            System.out.println("End of program");
+        }
+
+        //Should return "Done!"
+        try {
+            System.out.println(secondChallenge.probablyIWillThrowException(1.99, 2));
+        }
+        catch (Exception e) {
+            System.out.println("Program has an issue: " + e);
+        }
+        finally {
+            System.out.println("End of program");
+        }
+
+    }
+}
