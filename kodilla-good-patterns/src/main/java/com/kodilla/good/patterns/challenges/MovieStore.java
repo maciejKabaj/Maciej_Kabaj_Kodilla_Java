eac26b7366696a4a88b0419e1444ac8cb3c0219b
Maciej Kabaj <maciejkabaj93@gmail.com>
2025-07-05 19:28:11 +0200

    12.1 Added factorial calculation method with recursion and improved input handling in MovieStore class

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MovieStore.java
@@ -0,0 +1,40 @@
+package com.kodilla.good.patterns.challenges;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class MovieStore {
+
+    public String getMoviesSeparatedByExclamationMark() {
+        List<String> ironManTranslations = new ArrayList<>();
+        ironManTranslations.add("Żelazny Człowiek");
+        ironManTranslations.add("Iron Man");
+        List<String> avengersTranslations = new ArrayList<>();
+        avengersTranslations.add("Mściciele");
+        avengersTranslations.add("Avengers");
+        List<String> flashTranslations = new ArrayList<>();
+        flashTranslations.add("Błyskawica");
+        flashTranslations.add("Flash");
+        Map<String, List<String>> booksTitlesWithTranslations = new HashMap<>();
+        booksTitlesWithTranslations.put("IM", ironManTranslations);
+        booksTitlesWithTranslations.put("AV", avengersTranslations);
+        booksTitlesWithTranslations.put("FL", flashTranslations);
+
+        return booksTitlesWithTranslations.values().stream().
+                flatMap(List::stream).collect(Collectors.joining("!"));
+    }
+
+    public int countFactorial(int n) {
+        if (n == 0) return 1;
+        return n == 1 ? 1 : n * countFactorial(n - 1);
+    }
+
+    public static void main(String[] args) {
+        MovieStore movieStore = new MovieStore();
+        System.out.println(movieStore.getMoviesSeparatedByExclamationMark());
+        System.out.println(movieStore.countFactorial(4));
+    }
+}
