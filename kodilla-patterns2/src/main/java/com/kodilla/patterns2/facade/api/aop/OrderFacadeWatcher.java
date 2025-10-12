26c7048f975992f083c4c78b186bdfe5eb71466a
Maciej Kabaj <maciejkabaj93@gmail.com>
2025-10-12 12:33:58 +0200

    23.3 Added AOP aspect for logging OrderFacade processOrder() executions

---
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/facade/api/aop/OrderFacadeWatcher.java
@@ -0,0 +1,25 @@
+package com.kodilla.patterns2.facade.api.aop;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Aspect
+@Component
+public class OrderFacadeWatcher {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrderFacadeWatcher.class);
+
+    @Before("execution(* com.kodilla.patterns2.facade.api.OrderFacade.processOrder(..))")
+    public void logEvent(JoinPoint joinPoint) {
+        String methodName = joinPoint.getSignature().getName();
+        String className = joinPoint.getTarget().getClass().getSimpleName();
+        Object[] args = joinPoint.getArgs();
+        LOGGER.info("Class: " + className + ", Method: " + methodName + ", Args: " + Arrays.toString(args));
+    }
+}
